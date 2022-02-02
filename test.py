9c5a7ccf268a135f85df2ddce3e7e18c387573d4
S Karunakar Reddy <acece.skreddy@gmail.com>
2022-02-02 21:38:41 +0530

    #2

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-print("-"*20)
-print()
+print "-"*20
+print
 for i in range(5):
      for j in range(5-i):
-	      print("*",end = ' ')
-     print()
+	      print "*",end = ' '
+     print
 
-print()
-print("-"*20)
+print
+print "-"*20 
